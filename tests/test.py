32d6bdbf1e01a139487aec8599538fbd3431c0b9
clement-daubrenet <clement.daubrenet@gmail.com>
2018-07-02 01:58:15 +0100

    A very basic test

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+from threading import Thread
+import subprocess
+import sys
+
+SERVER_TIMEOUT_SECONDS = 15
+
+
+def server():
+    subprocess.run(['../server'], stdout=subprocess.PIPE)
+
+
+if __name__ == '__main__':
+    server = Thread(target=server)
+    server.start()
+    client = subprocess.Popen("../client", stdout=subprocess.PIPE, shell=True)
+    out, err = client.communicate()
+
+    # Checking that the client and the server return the right signals (parameters and file sent and received)
+
+    assert out == b'[CLIENT] Sending the string and the number to the server ... \n' \
+                  b'[SERVER] ...Received a string: test-string and a number: 17\n' \
+                  b'[CLIENT] Sending the file by chunk to the server... \n' \
+                  b'[SERVER] ...Finished to host the file\n'
+    sys.exit()
